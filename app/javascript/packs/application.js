c2ed95aa87aec655fbc05cbfb12d1fb4d1a934da
bondiano <batbondik0@gmail.com>
2021-07-05 23:20:49 +0300

    add simple layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'material-design-lite/material.js';
 
 Rails.start()
 ActiveStorage.start()
