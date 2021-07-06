8b69f96fcfa4bb5f49821e186147e6058bb072c4
bondiano <batbondik0@gmail.com>
2021-07-06 21:29:02 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost')
+admin.password = 'admin'
+admin.save
+
+60.times do |i|
+  u = [Manager, Developer].sample.new
+  u.email = "email#{i}@mail.gen"
+  u.first_name = "FN#{i}"
+  u.last_name = "LN#{i}"
+  u.password = i.to_s
+  u.save
+end
