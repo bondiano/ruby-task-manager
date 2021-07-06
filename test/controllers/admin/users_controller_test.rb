878fa604cf52347a03d1838c0f4d51a23c9dfa45
bondiano <batbondik0@gmail.com>
2021-07-06 20:53:30 +0300

    create and edit users

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -16,4 +16,28 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :show, params: { id: user.id }
     assert_response :success
   end
+
+  test "should get edit" do
+    user = create(:user)
+    get :edit, params: { id: user.id }
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should post create" do
+   user = attributes_for(:user)
+   post :create, params: { user: user }
+   assert_response :redirect
+ end
+
+ test "should patch update" do
+   user = create(:user)
+   user_attrs = attributes_for(:user)
+   patch :update, params: { id: user.id, user: user_attrs }
+   assert_response :redirect
+ end
 end
