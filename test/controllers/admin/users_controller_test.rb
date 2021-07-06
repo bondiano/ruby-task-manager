64ef23f8bb837c583d885d796f91bea7a15ed00c
bondiano <batbondik0@gmail.com>
2021-07-06 21:20:19 +0300

    delete user method

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -40,4 +40,10 @@ class Admin::UsersControllerTest < ActionController::TestCase
     patch :update, params: { id: user.id, user: user_attrs }
     assert_response :redirect
   end
+
+  test 'should delete destroy' do
+    user = create(:user)
+    delete :destroy, params: { id: user.id }
+    assert_response :redirect
+  end
 end
