acad34f1e18bdb981bb4a8aba357175f17779832
bondiano <batbondik0@gmail.com>
2021-07-06 12:39:30 +0300

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root :to => "web/boards#show"
 
+  resource :session, only: [:new, :create, :destroy]
+
   scope module: :web do
     resource :board, only: :show
     resource :session, only: :new
