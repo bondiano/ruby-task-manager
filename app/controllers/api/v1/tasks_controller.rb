92834a26a0ad0afa4e6352e4b20f5a726cfd32e1
bondiano <batbondik0@gmail.com>
2021-07-07 19:50:41 +0300

    add api

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,44 @@
+class Api:V1:TasksController < Api:V1:ApplicationController
+  def index
+    tasks = Task.all
+                .ransack(ransack_params)
+                .result
+                .page(page)
+                .per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+   def show
+    task = Task.find(params[:id])
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
+  end
+end
