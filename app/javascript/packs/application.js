91d0ce240e33cccbfe80243fa21e5f0538fd069e
bondiano <batbondik0@gmail.com>
2021-07-08 23:07:50 +0300

    Add js stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
 import 'material-design-lite/material.js';
 
-Rails.start()
-ActiveStorage.start()
+Rails.start();
+ActiveStorage.start();
